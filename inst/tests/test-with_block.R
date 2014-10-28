3ec4e2c29cab668674fdb93f0ee9a04aacf34f82
RobertZK <technoguyrob@gmail.com>
2014-10-28 18:09:40 -0500

    Add failing test for local passing in blocks

--- inst/tests/test-with_block.R
+++ inst/tests/test-with_block.R
@@ -5,3 +5,13 @@ test_that('it can perform a trivial block operation', {
   expect_equal(blocked_fn({ 1 + 1}), 2)
 })
 
+test_that('it can perform a slightly more complex block operation', {
+  blocked_fn <- with_block(function(x, y) x + y + yield())
+  expect_equal(blocked_fn(1, 2, { 3 + 4 }), 10)
+})
+
+test_that('it can execute a block with local passing', {
+  blocked_fn <- with_block(function(x, y) x + y + yield(z = 1))
+  expect_equal(blocked_fn(1, 2, { z + 1 }), 5)
+})
+
