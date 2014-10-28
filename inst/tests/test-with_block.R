c56cc96645049213f83ea0f8f4ec38913e8825dd
RobertZK <technoguyrob@gmail.com>
2014-10-28 18:33:27 -0500

    add test for checking we can execute a block with local passing referencing other locals

--- inst/tests/test-with_block.R
+++ inst/tests/test-with_block.R
@@ -15,6 +15,11 @@ test_that('it can execute a block with local passing', {
   expect_equal(blocked_fn(1, 2, { z + 1 }), 5)
 })
 
+test_that('it can execute a block with local passing referencing other locals', {
+  blocked_fn <- with_block(function(x, y) x + y + yield(z = x + 1))
+  expect_equal(blocked_fn(1, 2, { z + 1 }), 6)
+})
+
 test_that('it can upcase names as an example block usage', {
   assign_names <- with_block(function(x) {
    setNames(x, vapply(x, function(y) paste0("element_", yield(name = y)), character(1)))
